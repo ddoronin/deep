2c46b6e482a20d0b623e3c20c84175a97f4de8c4
Dmitry_Doronin <doronindm@gmail.com>
2017-02-06 22:40:03 -0500

    - set up PORT

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const cluster = require('cluster');
+const PORT = process.env.PORT || 3000;
+
 if (cluster.isMaster) {
 	const Master = require('./web/Master');
 	var master = new Master();
 	master.workers.forEach(_ => _.log());
 } else {
 	const Slave = require('./web/Slave');
-	new Slave(8080, (_, message) => _.send(`echo: ${message}`));
+	new Slave(PORT, (_, message) => _.send(`echo: ${message}`));
 }
