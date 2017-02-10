28a75049cfcb37208ef6e8990c08f5a96366e0a9
Dmitry_Doronin <doronindm@gmail.com>
2017-02-09 23:19:29 -0500

    - update path for static files

--- web/HttpServer.js
+++ web/HttpServer.js
@@ -1,6 +1,9 @@
 const _ = require('lodash');
 const path = require('path');
+const compression = require('compression');
 const serveStatic = require('serve-static');
+const helmet = require('helmet');
+const session = require('cookie-session')
 
 const setHeaders = (res, path) => {
 	if (serveStatic.mime.lookup(path) === 'text/html') {
@@ -11,12 +14,25 @@ const setHeaders = (res, path) => {
 
 class HttpServer {
 	constructor(app, wss, dirname) {
-		this.dist = path.join(dirname, 'public');
+		this.dist = path.resolve(dirname, './public');
+		console.log(`STATIC FILES ${this.dist}`);
 		this.app = app.use(serveStatic(this.dist, {
 			maxAge: '1d',
 			setHeaders
-		}));
+		})).use(compression()).use(helmet());
 		this.wss = wss;
+
+		this.app.disable('x-powered-by');
+		var expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
+		this.app.use(session({
+			name: 'session',
+			keys: ['key1', 'key2'],
+			cookie: {
+				secure: true,
+				httpOnly: true,
+				expires: expiryDate
+			}
+		}));
 	}
 }
 
