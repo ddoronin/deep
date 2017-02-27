523d94a78fac6c1b5a142d4e7e1d91af47b77890
Dmitry_Doronin <doronindm@gmail.com>
2017-02-27 00:01:44 -0500

    prepare web-dev-server for UI

--- ui/serve.js
+++ ui/serve.js
@@ -1,3 +1,34 @@
-/**
- * Created by Dmitry_Doronin on 2/12/2017.
- */
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const config = require('./webpack.config');
+const WebpackDevServer = require('webpack-dev-server');
+const open = require('open');
+
+const uiDevPort = 8080;
+const webPort = process.env.PORT || 9000;
+
+let server = new WebpackDevServer(webpack(config), {
+	contentBase: path.join(__dirname, './src'),
+	historyApiFallback: false,
+	hot: false,
+	port: uiDevPort,
+	noInfo: false,
+	proxy: {
+		'/api': {
+			target: `http://localhost:${webPort}`,
+			pathRewrite: {'^/api': ''}
+		}
+	}
+});
+
+// Listen to front-end changes
+server.listen(uiDevPort, 'localhost', (err) => {
+	if (err) {
+		return console.log(err);
+	}
+	console.log('Listening at localhost:' + uiDevPort);
+	console.log('Opening your system browser...');
+	open('http://localhost:' + uiDevPort + '/webpack-dev-server/');
+});
